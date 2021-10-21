23b2606350be7dd573c115a9e80872e046cc37bf
Amir Mohammad Sohrabi <sohrabi2557@gmail.com>
2021-10-21 11:06:51 +0330

    feat: add rest API

--- Kadoo/Backend_API/urls.py
+++ Kadoo/Backend_API/urls.py
@@ -1,5 +1,28 @@
 from django.urls import path
+from . import views
 
 urlpatterns=[
-    
+    path('', views.apiOverview, name='api'),
+
+# Plant API
+    path('plantList/', views.plantList, name='plantList'),
+    path('plantDetail/<str:pk>/', views.plantDetail, name='plantDetail'),
+    path('createPlant/', views.createPlant, name='createPlant'),
+    path('updatePlant/<str:pk>/', views.updatePlant, name='updatePlant'),
+    path('deletePlant/<str:pk>/', views.deletePlant, name='deletePlant'),
+
+# Tool API
+    path('toolList/', views.toolList, name='toolList'),
+    path('toolDetail/<str:pk>/', views.toolDetail, name='toolDetail'),
+    path('createTool/', views.createTool, name='createTool'),
+    path('updateTool/<str:pk>/', views.updateTool, name='updateTool'),
+    path('deleteTool/<str:pk>/', views.deleteTool, name='deleteTool'),
+
+# Tag API
+    path('tagList/', views.tagList, name='tagList'),
+    path('tagDetail/<str:pk>/', views.tagDetail, name='tagDetail'),
+    path('createTag/', views.createTag, name='createTag'),
+    path('updateTag/<str:pk>/', views.updateTag, name='updateTag'),
+    path('deleteTag/<str:pk>/', views.deleteTag, name='deleteTag'),
+
 ]
--- Kadoo/Backend_API/views.py
+++ Kadoo/Backend_API/views.py
@@ -1,3 +1,145 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import PlantSerializer, ToolSerializer, TagSerializer
+from Backend.models import Plant, Tool, Tag
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'plantsList':'/plantList/',
+        'plantDetail':'/plantDetail/<str:pk>/',
+        'createPlant':'/createPlant/',
+        'updatePlant':'/updatePlant/<str:pk>/',
+        'deletePlant':'/deletePlant/<str:pk>/',
+
+        'toolsList':'/toolList/',
+        'toolDetail':'/toolDetail/<str:pk>/',
+        'createTool':'/createTool/',
+        'updateTool':'/updateTool/<str:pk>/',
+        'deleteTool':'/deleteTool/<str:pk>/',
+
+        'tagsList':'/tagList/',
+        'tagDetail':'/tagDetail/<str:pk>/',
+        'createTag':'/createTag/',
+        'updateTag':'/updateTag/<str:pk>/',
+        'deleteTag':'/deleteTag/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+# Plant
+@api_view(['GET'])
+def plantList(request):
+    plants = Plant.objects.all()
+    serializer = PlantSerializer(plants, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def plantDetail(request, pk):
+    plants = Plant.objects.get(id=pk)
+    serializer = PlantSerializer(plants, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createPlant(request):
+    serializer = PlantSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updatePlant(request, pk):
+    plant = Plant.objects.get(id=pk)
+    serializer = PlantSerializer(instance = plant, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deletePlant(request, pk):
+    plant = Plant.objects.get(id=pk)
+    plant.delete()
+    return Response('Item successfully deleted !')
+
+# Tool
+@api_view(['GET'])
+def toolList(request):
+    tools = Tool.objects.all()
+    serializer = ToolSerializer(tools, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def toolDetail(request, pk):
+    tools = Tool.objects.get(id=pk)
+    serializer = ToolSerializer(tools, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createTool(request):
+    serializer = ToolSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateTool(request, pk):
+    tool = Tool.objects.get(id=pk)
+    serializer = ToolSerializer(instance = tool, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteTool(request, pk):
+    tool = Tool.objects.get(id=pk)
+    tool.delete()
+    return Response('Item successfully deleted !')
+
+# Tag
+@api_view(['GET'])
+def tagList(request):
+    tags = Tag.objects.all()
+    serializer = TagSerializer(tags, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tagDetail(request, pk):
+    tags = Tag.objects.get(id=pk)
+    serializer = TagSerializer(tags, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createTag(request):
+    serializer = TagSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateTag(request, pk):
+    tag = Tag.objects.get(id=pk)
+    serializer = TagSerializer(instance = tag, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteTag(request, pk):
+    tag = Tag.objects.get(id=pk)
+    tag.delete()
+    return Response('Item successfully deleted !')
