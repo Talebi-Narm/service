2b5ddf46cdd23181df9e9460056bd317cdb587ab
Amir Mohammad <sohrabi2557@gmail.com>
2021-11-11 01:09:13 +0330

    feat: init models

--- Kadoo/Green_House/admin.py
+++ Kadoo/Green_House/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import  myPlant
 
-# Register your models here.
+admin.site.register(myPlant)
--- Kadoo/Green_House/models.py
+++ Kadoo/Green_House/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Backend.models import Plant
+from Users.models import NewUser
+import uuid
 
-# Create your models here.
+class myPlant(models.Model):
+    id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = False)
+    user = models.ForeignKey('Users.NewUser', on_delete=models.CASCADE)
+    plant = models.ForeignKey('Backend.Plant', on_delete=models.CASCADE)
+    image = models.ImageField(null = True, blank = True)
+    location = models.CharField(max_length=50, null=True, blank=True)
+    isArchived = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add = True, blank=True)
+    modified = models.DateField(auto_now = True, blank=True)
+
+    def __str__(self):
+        return  "{0} {1}".format(self.plant.name, self.user)
