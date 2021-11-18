eb06aba4e76ee0a9e211e2a3779502eb503c3927
Amir Mohammad <sohrabi2557@gmail.com>
2021-11-18 19:24:35 +0330

    feat: change some API

--- Kadoo/Green_House/views.py
+++ Kadoo/Green_House/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import get_object_or_404
 
+from rest_framework import status
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import myPlant
-from .serializers import myPlantSerializer
+from Backend.models import Plant
+from .serializers import *
+
+import uuid
 
 def GH_Overview():
     api_urls = {
         'see green house plants':'/myPlants/',
         'see green house plants':'/myArchivedPlants/',
         'add to green house plants':'/addToMyPlants/',
+        'get a plant information':'/getMyPlant/<str:pk>/',
         'update plant in green house':'/updateInMyPlants/<str:pk>/',
     }
     return api_urls
@@ -20,7 +26,7 @@ def allOfMyPlant(request):
     if request.user.is_anonymous:
         return Response("Anonymous User: You should first login.", status=status.HTTP_401_UNAUTHORIZED)
     _user = request.user
-    myPlants = myPlant.objects.filter(user = _user).filter(isArchived = False)
+    myPlants = myPlant.objects.filter(user = _user, isArchived = False)
     serializer = myPlantSerializer(myPlants, many=True)
     return Response(serializer.data)
 
@@ -29,15 +35,34 @@ def allOfMyArchivedPlant(request):
     if request.user.is_anonymous:
         return Response("Anonymous User: You should first login.", status=status.HTTP_401_UNAUTHORIZED)
     _user = request.user
-    myPlants = myPlant.objects.filter(user = _user).filter(isArchived = True)
+    myPlants = myPlant.objects.filter(user = _user, isArchived = True)
     serializer = myPlantSerializer(myPlants, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def addPlantToMyGreenHouse(request):
-    serializer = myPlantSerializer(data=request.data)
+    serializer = addPlantSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        if request.user.is_anonymous:
+            return Response("Anonymous User: You should first login.", status=status.HTTP_401_UNAUTHORIZED)
+        if Plant.objects.filter(id=serializer.data["plant"]).exists() == False:
+            return response.Response("This plant does NOT Exist!", status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        _user = request.user
+        _plant = Plant.objects.get(id=serializer.data['plant'])
+        try :
+            _location = location=serializer.data['location']
+        except:
+            _location = None
+        _myPlant = myPlant.objects.create(user=_user, plant=_plant, location=serializer.data['location'])
+        _myPlant.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getPlant(request, pk):
+    if request.user.is_anonymous:
+        return Response("Anonymous User: You should first login.", status=status.HTTP_401_UNAUTHORIZED)
+    _myPlant = get_object_or_404(myPlant, id=pk)
+    serializer = myPlantSerializer(_myPlant, many=False)
     return Response(serializer.data)
 
 @api_view(['POST'])
--- Kadoo/Kadoo/settings.py
+++ Kadoo/Kadoo/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
