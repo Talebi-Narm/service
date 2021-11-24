604e244913547604c58872f83890acdda7a89f16
Amir Mohammad <sohrabi2557@gmail.com>
2021-11-24 15:36:01 +0330

    pref: change pass parameters in url to POST method

--- Kadoo/Backend_API/serializers.py
+++ Kadoo/Backend_API/serializers.py
@@ -26,46 +26,87 @@ class AlbumSerializer(serializers.ModelSerializer):
         model = Album
         fields = '__all__'
 
+class paginatorSerializer(serializers.Serializer):
+    count = serializers.IntegerField(required=True)
+    page = serializers.IntegerField(required=True)
+
+class sortSerializer(serializers.Serializer):
+    kind = serializers.ChoiceField(required=True,
+        choices = ['name', 'price', 'time']
+    )
+    order = serializers.ChoiceField(required=False, default='ASC',
+        choices = ['ASC', 'DES']
+    )
+
 class nameSerializer(serializers.Serializer):
     name = serializers.CharField(required=True)
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
     
 class priceSerializer(serializers.Serializer):
     lower = serializers.IntegerField(required=False, default=0)
     higher = serializers.IntegerField(required=False, default=inf)
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
     
 class environmentSerializer(serializers.Serializer):
     environment = serializers.ChoiceField(required=True,
         choices = ['cold', 'tropical', 'none']
     )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
     
 class waterSerializer(serializers.Serializer):
     water = serializers.ChoiceField(required=True,
         choices = ['low', 'medium', 'much']
     )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
     
 class lightSerializer(serializers.Serializer):
     light = serializers.ChoiceField(required=True,
         choices = ['low', 'medium', 'much']
     )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
 class growthRateSerializer(serializers.Serializer):
     growthRate = serializers.ChoiceField(required=True,
         choices = ['low', 'medium', 'much']
     )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
 
-class plantAdvanceSerializer(serializers.Serializer):
-    name = nameSerializer(required=False, default=None)
-    price = priceSerializer(required=False, default=None)
-    environment = environmentSerializer(required=False, default=None)
-    water = waterSerializer(required=False, default=None)
-    light = lightSerializer(required=False, default=None)
-    growthRate = growthRateSerializer(required=False, default=None)
+class tagsSerializer(serializers.Serializer):
     tags = serializers.ListField(required=False, default=[],
         child = serializers.CharField(required=True)
     )
+
+class plantAdvanceSerializer(serializers.Serializer):    
+    name = serializers.CharField(required=False, default=None)
+    price = priceSerializer(required=False, default=None)
+    environment = serializers.ChoiceField(required=False, default=None,
+        choices = ['cold', 'tropical', 'none']
+    )
+    water = serializers.ChoiceField(required=False, default=None,
+        choices = ['low', 'medium', 'much']
+    )
+    light = serializers.ChoiceField(required=False, default=None,
+        choices = ['low', 'medium', 'much']
+    )
+    growthRate = serializers.ChoiceField(required=False, default=None,
+        choices = ['low', 'medium', 'much']
+    )
+    tags = serializers.ListField(required=False, default=[],
+        child = serializers.CharField(required=True)
+    )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
 
 class toolAdvanceSerializer(serializers.Serializer):
-    name = nameSerializer(required=False, default=None)
+    name = serializers.CharField(required=False, default=None)
     price = priceSerializer(required=False, default=None)
     tags = serializers.ListField(required=False, default=[],
         child = serializers.CharField(required=True)
     )
+    pagination = paginatorSerializer(required=False, default=None)
+    sort = sortSerializer(required=False, default=None)
--- Kadoo/Backend_API/urls.py
+++ Kadoo/Backend_API/urls.py
@@ -47,32 +47,32 @@ urlpatterns=[
     path('addImageToAlbum/<str:pk>/', views.createImage, name='addImageToAlbum'),
 
 # searching and filtering for plants
-    path('plantsByName/<str:_name>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByName, name='plantsByName'),
-    path('plantsByPrice/<str:prices>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByPrice, name='plantsByPrice'),
-    path('plantsByEnvironment/<str:_environment>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByEnvironment, name='plantsByEnvironment'),
-    path('plantsByWater/<str:_water>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByWater, name='plantsByWater'),
-    path('plantsByLight/<str:_light>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByLight, name='plantsByLight'),
-    path('plantsByGrowthRate/<str:_growthRate>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByGrowthRate, name='plantsByGrowthRate'),
-    path('plantsByTags/<str:tags>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsByTags, name='plantsByTags'),
+    path('plantsByName/', sfsp_view.plantsByName, name='plantsByName'),
+    path('plantsByPrice/', sfsp_view.plantsByPrice, name='plantsByPrice'),
+    path('plantsByEnvironment/', sfsp_view.plantsByEnvironment, name='plantsByEnvironment'),
+    path('plantsByWater/', sfsp_view.plantsByWater, name='plantsByWater'),
+    path('plantsByLight/', sfsp_view.plantsByLight, name='plantsByLight'),
+    path('plantsByGrowthRate/', sfsp_view.plantsByGrowthRate, name='plantsByGrowthRate'),
+    path('plantsByTags/', sfsp_view.plantsByTags, name='plantsByTags'),
 
 # searching and filtering for tools
-    path('toolsByName/<str:_name>/<str:_paginator>/<str:_sorting>/', sfsp_view.toolsByName, name='toolsByName'),
-    path('toolsByPrice/<str:prices>/<str:_paginator>/<str:_sorting>/', sfsp_view.toolsByPrice, name='toolsByPrice'),
-    path('toolsByTags/<str:tags>/<str:_paginator>/<str:_sorting>/', sfsp_view.toolsByTags, name='toolsByTags'),
+    path('toolsByName/', sfsp_view.toolsByName, name='toolsByName'),
+    path('toolsByPrice/', sfsp_view.toolsByPrice, name='toolsByPrice'),
+    path('toolsByTags/', sfsp_view.toolsByTags, name='toolsByTags'),
 
 # plants sorting
-    path('plantsSortByName/<str:kind>/', sfsp_view.plantsSortByName, name='plantsSortByName'),
-    path('plantsSortByPrice/<str:kind>/', sfsp_view.plantsSortByPrice, name='plantsSortByPrice'),
+    path('plantsSortByName/', sfsp_view.plantsSortByName, name='plantsSortByName'),
+    path('plantsSortByPrice/', sfsp_view.plantsSortByPrice, name='plantsSortByPrice'),
     path('plantsSortByNewest/', sfsp_view.plantsSortByCreateDate, name='plantsSortByCreateDate'),
 
 # tools sorting
-    path('toolsSortByName/<str:kind>/', sfsp_view.toolsSortByName, name='toolsSortByName'),
-    path('toolsSortByPrice/<str:kind>/', sfsp_view.toolsSortByPrice, name='toolsSortByPrice'),
+    path('toolsSortByName/', sfsp_view.toolsSortByName, name='toolsSortByName'),
+    path('toolsSortByPrice/', sfsp_view.toolsSortByPrice, name='toolsSortByPrice'),
     path('toolsSortByNewest/', sfsp_view.toolsSortByCreateDate, name='toolsSortByCreateDate'),
 
 # advance search
-    path('plantsAdvanceSearch/<str:filters>/<str:_paginator>/<str:_sorting>/', sfsp_view.plantsAdvanceSearch, name='plantsAdvanceSearch'),
-    path('toolsAdvanceSearch/<str:filters>/<str:_paginator>/<str:_sorting>/', sfsp_view.toolsAdvanceSearch, name='toolsAdvanceSearch'),
+    path('plantsAdvanceSearch/', sfsp_view.plantsAdvanceSearch, name='plantsAdvanceSearch'),
+    path('toolsAdvanceSearch/', sfsp_view.toolsAdvanceSearch, name='toolsAdvanceSearch'),
 
 # category API
     path('plantsWithTag/<str:tag_name>/', views.plantsWithSpecificTag, name='plantsWithTag'),
