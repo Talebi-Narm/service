1c1d7e58ceb02906287e58de2e33f40ae07d1494
Amir Mohammad <sohrabi2557@gmail.com>
2021-12-22 11:48:00 +0330

    impro: transform views.py in Green_House folder to class-based API

--- Kadoo/Green_House/urls.py
+++ Kadoo/Green_House/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('myPlants/', views.allOfMyPlant, name='allOfMyPlants'),
-    path('myArchivedPlants/', views.allOfMyArchivedPlant, name='allOfMyArchivedPlants'),
-    path('addToMyPlants/', views.addPlantToMyGreenHouse, name='addPlantToMyGreenHouse'),
-    path('getMyPlant/<str:pk>/', views.getPlant, name='getPlant'),
-    path('updateInMyPlants/<str:pk>/', views.updatePlantInMyGreenHouse, name='updatePlantInMyGreenHouse'), 
+    path('myPlants/', views.allOfMyPlant.as_view(), name='allOfMyPlants'),
+    path('myArchivedPlants/', views.allOfMyArchivedPlant.as_view(), name='allOfMyArchivedPlants'),
+    path('addToMyPlants/', views.addPlantToMyGreenHouse.as_view(), name='addPlantToMyGreenHouse'),
+    path('MyPlants/<str:pk>/', views.myPlants.as_view(), name='getPlant'),
 ]
