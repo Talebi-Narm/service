6e97a50c935ba231af5681c6efc054df068d062f
Amir Mohammad <sohrabi2557@gmail.com>
2021-12-26 12:16:43 +0330

    feat: add API to archive a plant using ID and improve some API

--- Kadoo/Green_House/urls.py
+++ Kadoo/Green_House/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns=[
     path('myPlants/', views.allOfMyPlant.as_view(), name='allOfMyPlants'),
     path('myArchivedPlants/', views.allOfMyArchivedPlant.as_view(), name='allOfMyArchivedPlants'),
-    path('addToMyPlants/', views.addPlantToMyGreenHouse.as_view(), name='addPlantToMyGreenHouse'),
-    path('MyPlants/<str:pk>/', views.myPlants.as_view(), name='getPlant'),
+    path('myPlantsRUD/<str:pk>/', views.myPlantsRUD.as_view(), name='getPlant'),
 ]
