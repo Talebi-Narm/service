5c563132dbc178393c8277dc1e313474302285e3
Navid.MSZD <s.navidm1380@gmail.com>
2021-12-28 12:36:13 +0330

    fix: CRD specialist

--- Kadoo/Specialist/urls.py
+++ Kadoo/Specialist/urls.py
@@ -9,8 +9,10 @@ urlpatterns=[
     path('all-primary/', views.GetAllSpecialistPrimaryInfo.as_view(), name='specialist-all-primary'),
     path('all-secondary/', views.GetAllSpecialistSecondaryInfo.as_view(), name='specialist-all-secondary'),
     path('primary/', views.GetThisSpecialistPrimaryInfo.as_view(), name='specialist-primary'),
-    path('secondary/', views.GetThisSpecialistPrimaryInfo.as_view(), name='specialist-secondary'),
-    path('secondary/<str:pk>/', views.GetSpecialistIdPrimaryInfo.as_view(), name='specialist-secondary-id'),
-    path('update-secondary/', views.UpdateSpecialistInfo.as_view(), name='specialist-update-secondary'),
+    path('secondary/', views.GetThisSpecialistSecondaryInfo.as_view(), name='specialist-secondary'),
+    path('secondary/<str:pk>/', views.GetSpecialistIdSecondaryInfo.as_view(), name='specialist-secondary-id'),
+    path('primary/<str:pk>/', views.GetSpecialistIdPrimaryInfo.as_view(), name='specialist-secondary-id'),
+    path('update-secondary/<str:pk>/', views.UpdateSpecialistInfo.as_view(), name='specialist-update-secondary'),
+    path('update-primary/<str:pk>/', views.UpdatePrimarySpecialistInfo.as_view(), name='specialist-update-secondary'),
     path('delete/', views.RemoveSpecialist.as_view(), name='specialist-delete'),
 ]
