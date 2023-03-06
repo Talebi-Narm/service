73076264ca551572e423573e7df2540ddc42d0f2
Hamed Feizabadi <hamedfz19@gmail.com>
2023-03-06 16:37:47 +0330

    fix: some urls & folder structures

--- Kadoo/urls.py
+++ Kadoo/urls.py
@@ -6,8 +6,10 @@ from rest_framework_swagger.views import get_swagger_view
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import generics
 
+
 class AuthTokenView(ObtainAuthToken, generics.GenericAPIView):
     pass
+
 
 schema_view = get_swagger_view(title='Kadoo Project Swagger')
 
@@ -19,13 +21,13 @@ urlpatterns = [
     path('api/', include('Backend_API.urls')),
     path('api/', include('Green_House.urls')),
     path('api/', include('Reminder.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework_CHANGE_THIS')),
     path('api/user/', include('Users.urls', namespace='users')),
     path('api/cart/', include('Cart.urls', namespace='cart')),
     path('api/specialist/', include('Specialist.urls', namespace='specialist')),
     path('api/ticket/', include('Ticket.urls', namespace='ticket')),
     path('api/coin/', include('Coin.urls', namespace='coin')),
-    path(r'^api-token-auth/', AuthTokenView.as_view())
+    path('api-token-auth/', AuthTokenView.as_view())
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
