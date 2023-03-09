a4999e1fd7e2b9832b8c54261c19cfd3aa2ecbde
Amir Mohammad <sohrabi2557@gmail.com>
2023-03-09 12:45:15 +0330

    feat: edit urls path

--- Kadoo/urls.py
+++ Kadoo/urls.py
@@ -11,18 +11,17 @@ class AuthTokenView(ObtainAuthToken, generics.GenericAPIView):
 
 
 urlpatterns = [
-    path('', include('Backend.urls')),
     path('accounts/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('Backend_API.urls')),
-    path('api/', include('Green_House.urls')),
-    path('api/', include('Reminder.urls')),
+    path('core/', include('core.urls')),
+    path('green-house/', include('Green_House.urls')),
+    path('reminder/', include('Reminder.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework_CHANGE_THIS')),
-    path('api/user/', include('Users.urls', namespace='users')),
-    path('api/cart/', include('Cart.urls', namespace='cart')),
-    path('api/specialist/', include('Specialist.urls', namespace='specialist')),
-    path('api/ticket/', include('Ticket.urls', namespace='ticket')),
-    path('api/coin/', include('Coin.urls', namespace='coin')),
+    path('user/', include('Users.urls', namespace='users')),
+    path('cart/', include('Cart.urls', namespace='cart')),
+    path('specialist/', include('Specialist.urls', namespace='specialist')),
+    path('ticket/', include('Ticket.urls', namespace='ticket')),
+    path('coin/', include('Coin.urls', namespace='coin')),
     path('api-token-auth/', AuthTokenView.as_view())
 ]
 
