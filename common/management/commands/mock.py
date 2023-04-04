b7f6505acf03bca0d89378a663b152034ba06bd8
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-04 17:51:31 +0430

    feat: create atomic handle function

--- common/management/commands/mock.py
+++ common/management/commands/mock.py
@@ -1,3 +1,8 @@
 from django.core.management.base import BaseCommand
+
+from django.db import transaction
 class Command(BaseCommand):
     help = "generate fake data"
+
+    @transaction.atomic
+    def handle(self, *args, **options):
