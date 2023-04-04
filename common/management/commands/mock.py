26499c7d2d18bfa6149a382d37313a20aad5ab14
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-04 17:52:01 +0430

    feat: add faker

--- common/management/commands/mock.py
+++ common/management/commands/mock.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
+from faker import Faker
 
 from django.db import transaction
+
 class Command(BaseCommand):
     help = "generate fake data"
 
     @transaction.atomic
     def handle(self, *args, **options):
+        faker = Faker()
