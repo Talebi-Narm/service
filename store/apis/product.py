f52b4deb14b8212a53d6e3db4ee4981b420a4cc0
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-18 16:03:03 +0430

    fix: add missing queryset

--- store/apis/product.py
+++ store/apis/product.py
@@ -6,22 +6,26 @@ from common.utils.paginator import *
 
 # ----> PLANT <----
 class PlantList(generics.ListAPIView):
+    queryset = Plant.objects.all()
     serializer_class = PlantSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
 
 class PlantDetail(generics.RetrieveAPIView):
+    queryset = Plant.objects.all()
     serializer_class = PlantSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
 
 # ----> TOOL <----
 class ToolList(generics.ListAPIView):
+    queryset = Tool.objects.all()
     serializer_class = ToolSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
 
 class ToolDetail(generics.RetrieveAPIView):
+    queryset = Tool.objects.all()
     serializer_class = ToolSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
