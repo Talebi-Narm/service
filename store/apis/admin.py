84c8bb9f43f2ef9df92646675ec347ced1baab9d
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-18 16:13:20 +0430

    fix: add missing quertset

--- store/apis/admin.py
+++ store/apis/admin.py
@@ -5,21 +5,25 @@ from store.serializers.product import *
 from common.utils.paginator import *
 
 class PlantList(generics.ListCreateAPIView):
+    queryset = Plant.objects.all()
     serializer_class = PlantAdminSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAdminUser]
 
 class PlantDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Plant.objects.all()
     serializer_class = PlantAdminSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAdminUser]
 
 class ToolList(generics.ListCreateAPIView):
+    queryset = Tool.objects.all()
     serializer_class = ToolAdminSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAdminUser]
 
 class ToolDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tool.objects.all()
     serializer_class = ToolAdminSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAdminUser]
