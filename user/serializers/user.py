c7af3288434c7ea7dad9da547609a9c271e08ca9
Hamed Feizabadi <hamedfz19@gmail.com>
2023-04-20 20:12:25 +0330

    refactor: linter issues for user

--- user/serializers/user.py
+++ user/serializers/user.py
@@ -7,8 +7,8 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         exclude = (
-            'is_staff', 'is_superuser', 'groups', 'user_permissions', 'created_at', 'deleted_at', 'updated_at', 'is_deleted',
-            'is_active', 'calendar_id', 'calendar_token', 'phone_number')
+            'is_staff', 'is_superuser', 'groups', 'user_permissions', 'created_at', 'deleted_at', 'updated_at',
+            'is_deleted', 'is_active', 'calendar_id', 'calendar_token', 'phone_number')
         read_only_fields = ('id', 'last_login', 'date_joined', 'wallet_charge')
         extra_kwargs = {
             'password': {'write_only': True},
