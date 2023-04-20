5902b4f285f3038f8424d132b63eb1043c30ca3c
Hamed Feizabadi <hamedfz19@gmail.com>
2023-04-20 19:07:20 +0330

    feat: user address APIs
    feat: user address APIs

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,17 @@
-from rest_framework_simplejwt import views
-from user.apis.register import RegisterUserAPIView
 from django.urls import path
+from rest_framework_simplejwt import views
+
+from user.apis.address import UserAddressList, UserAddressDetail
+from user.apis.register import RegisterUserAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path("jwt/create/", views.TokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", views.TokenRefreshView.as_view(), name="jwt-refresh"),
-    path("register", RegisterUserAPIView.as_view(), name="new-user-register")
+    path("register", RegisterUserAPIView.as_view(), name="new-user-register"),
+    path("address", UserAddressList.as_view(), name="user-address-list"),
+    path("address/<uuid:pk>", UserAddressDetail.as_view(), name="user-address-detail"),
 
     # path('', views.apiOverview, name='api'),
     # path('updatecredit/<int:amount>/', UpdateCredit.as_view(), name='update_credit'),
