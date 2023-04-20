b5cd4f273ba3df9617062f3f1936a689dd5f0bae
Hamed Feizabadi <hamedfz19@gmail.com>
2023-04-20 19:24:16 +0330

    fix: addresses urls

--- user/urls.py
+++ user/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path("jwt/create/", views.TokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", views.TokenRefreshView.as_view(), name="jwt-refresh"),
     path("register", RegisterUserAPIView.as_view(), name="new-user-register"),
-    path("address", UserAddressList.as_view(), name="user-address-list"),
-    path("address/<uuid:pk>", UserAddressDetail.as_view(), name="user-address-detail"),
+    path("addresses", UserAddressList.as_view(), name="user-address-list"),
+    path("addresses/<uuid:pk>", UserAddressDetail.as_view(), name="user-address-detail"),
 
     # path('', views.apiOverview, name='api'),
     # path('updatecredit/<int:amount>/', UpdateCredit.as_view(), name='update_credit'),
