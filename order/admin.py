a3e134542d70c1aacd160ea6143af8aa02747c05
Hamed Feizabadi <hamedfz19@gmail.com>
2023-04-21 15:32:19 +0330

    fix: discount min and max validator + panel admin

--- order/admin.py
+++ order/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import Coupon
 
 # Register your models here.
-admin.register(Coupon)
+admin.site.register(Coupon)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from common.models import BaseModel
@@ -10,7 +11,12 @@ from common.models import BaseModel
 class Coupon(BaseModel):
     title = models.CharField(max_length=100, unique=True)
     code = models.CharField(max_length=100, unique=True)
-    discount = models.IntegerField()
+    discount = models.IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     count = models.PositiveIntegerField(null=True, blank=True)  # null for infinite
