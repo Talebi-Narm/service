2faf7089befc54ccfe9146df1ce799b8d76361dc
Hamed Feizabadi <hamedfz19@gmail.com>
2023-04-21 14:48:44 +0330

    feat: coupon model def

--- order/models.py
+++ order/models.py
@@ -1,9 +1,16 @@
+from django.db import models
+
 from common.models import BaseModel
 
 
-class Order(BaseModel):
-    pass
+# class Order(BaseModel):
+#     pass
 
 
 class Coupon(BaseModel):
-    pass
+    title = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=100, unique=True)
+    discount = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    count = models.PositiveIntegerField(null=True, blank=True)  # null for infinite
