7d0fd70ba606180c4d89cab104a3861d1aea1358
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-21 23:35:24 +0330

    feat: register on admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from store.models import Plant, Tool
+from store.models import Plant, Tool, PlantComment, ToolComment
 
 # Register your models here.
 admin.site.register(Plant)
 admin.site.register(Tool)
+admin.site.register(PlantComment)
+admin.site.register(ToolComment)
