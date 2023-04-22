b8e68cf17f959253b722fb887cbf8cb82c14fd5f
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-22 09:36:09 +0330

    fix: edit serializers

--- store/apis/comment.py
+++ store/apis/comment.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 from common.utils.paginator import ResponsePagination
 from store.models import Plant, Tool, PlantComment, ToolComment
-from store.serializers.comment import CommentSerializer
+from store.serializers.comment import PlantCommentSerializer, ToolCommentSerializer
 
 
 class PlantCommentAdminApprove(GenericAPIView):
@@ -16,7 +16,7 @@ class PlantCommentAdminApprove(GenericAPIView):
         comment = get_object_or_404(PlantComment, id=pk)
         comment.is_active = True
         comment.save()
-        return Response({"date": "Done!"}, status=status.HTTP_200_OK)
+        return Response({"message": "Done!"}, status=status.HTTP_200_OK)
 
 
 class ToolCommentAdminApprove(GenericAPIView):
@@ -26,19 +26,19 @@ class ToolCommentAdminApprove(GenericAPIView):
         comment = get_object_or_404(ToolComment, id=pk)
         comment.is_active = True
         comment.save()
-        return Response({"date": "Done!"}, status=status.HTTP_200_OK)
+        return Response({"message": "Done!"}, status=status.HTTP_200_OK)
 
 
 class PlantCommentCreate(generics.CreateAPIView):
     queryset = PlantComment.objects.all()
-    serializer_class = CommentSerializer
+    serializer_class = PlantCommentSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
 
 
 class ToolCommentCreate(generics.CreateAPIView):
     queryset = PlantComment.objects.all()
-    serializer_class = CommentSerializer
+    serializer_class = ToolCommentSerializer
     pagination_class = ResponsePagination
     permission_classes = [IsAuthenticated]
 
@@ -47,7 +47,7 @@ class PlantComments(GenericAPIView):
     def get(self, request, pk):
         plant = get_object_or_404(Plant, id=pk)
         comments = PlantComment.objects.filter(plant__id=plant.id)
-        serializer = CommentSerializer(comments, many=True)
+        serializer = PlantCommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -55,5 +55,5 @@ class ToolComments(GenericAPIView):
     def get(self, request, pk):
         plant = get_object_or_404(Tool, id=pk)
         comments = PlantComment.objects.filter(plant__id=plant.id)
-        serializer = CommentSerializer(comments, many=True)
+        serializer = ToolCommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
