d7d3569220c966a357bcdcfc7f6457d3c91ac419
Amir Mohammad <sohrabi2557@gmail.com>
2023-04-22 11:43:00 +0330

    fix: remove extra apis

--- store/apis/comment.py
+++ store/apis/comment.py
@@ -1,32 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from common.utils.paginator import ResponsePagination
 from store.models import Plant, Tool, PlantComment, ToolComment
 from store.serializers.comment import PlantCommentSerializer, ToolCommentSerializer
-
-
-class PlantCommentAdminApprove(GenericAPIView):
-    permission_classes = [IsAdminUser]
-
-    def put(self, request, pk):
-        comment = get_object_or_404(PlantComment, id=pk)
-        comment.is_active = True
-        comment.save()
-        return Response({"message": "Done!"}, status=status.HTTP_200_OK)
-
-
-class ToolCommentAdminApprove(GenericAPIView):
-    permission_classes = [IsAdminUser]
-
-    def put(self, request, pk):
-        comment = get_object_or_404(ToolComment, id=pk)
-        comment.is_active = True
-        comment.save()
-        return Response({"message": "Done!"}, status=status.HTTP_200_OK)
 
 
 class PlantCommentCreate(generics.CreateAPIView):
