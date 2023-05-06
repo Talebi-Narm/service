9ddb202269780f9407dd0de4f482ac91eca27db5
Hamed Feizabadi <hamedfz19@gmail.com>
2023-05-06 15:25:54 +0330

    feat: green house test

--- green_house/urls.py
+++ green_house/urls.py
@@ -4,5 +4,5 @@ from . import apis
 
 urlpatterns = [
     path('user-plants/', apis.UserPlantListAPIView.as_view(), name='user-plants'),
-    path('user-plants/<uuid:pk>/', apis.UserPlantDetailAPIView.as_view(), name='user-plant-detail'),
+    path('user-plants/<uuid:pk>/', apis.UserPlantDetailAPIView.as_view(), name='user-plants-detail'),
 ]
