32d948ebb97ffe0511abbf0148de0d6aff7d5569
Hamed Feizabadi <hamedfz19@gmail.com>
2023-05-06 16:35:31 +0330

    fix: user profile schema

--- user/apis/user.py
+++ user/apis/user.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,9 +8,13 @@ from rest_framework.response import Response
 from user.models import User, UserAddress
 from user.serializers.user import UserProfileSerializer
 
+
 class UserProfile(GenericAPIView):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        responses={200: UserProfileSerializer}
+    )
     def get(self, request):
         pk = request.user.id
         user = get_object_or_404(User, id=pk)
@@ -18,4 +24,4 @@ class UserProfile(GenericAPIView):
         result['user'] = user
         result['addresses'] = user_addresses
         serializer = UserProfileSerializer(result, many=False)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
