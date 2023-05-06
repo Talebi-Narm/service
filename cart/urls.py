7eb3b0c7d8c9a3037052754d7358d6449fa7549f
Hamed Feizabadi <hamedfz19@gmail.com>
2023-05-06 16:22:48 +0330

    fix: cart urls

--- cart/urls.py
+++ cart/urls.py
@@ -6,7 +6,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('plant-cart', cart_views.PlantCartList.as_view(), name="plant-cart"),
-    path('plant-cart/<uuid:pk>/', cart_views.PlantCartList.as_view(), name="plant-cart"),
+    path('plant-cart/<uuid:pk>/', cart_views.PlantCartDetail.as_view(), name="plant-cart-detail"),
     path('tool-cart', cart_views.ToolCartList.as_view(), name="tool-cart"),
-    path('tool-cart/<uuid:pk>/', cart_views.ToolCartList.as_view(), name="tool-cart"),
+    path('tool-cart/<uuid:pk>/', cart_views.ToolCartDetail.as_view(), name="tool-cart-detail"),
 ]
