f8e67e6de8b30c1f723a6d3fea60ee6ccc15689c
Amir Mohammad <sohrabi2557@gmail.com>
2023-06-04 18:01:19 +0330

    feat: show full info instead of just id for owner and reply_to

--- store/serializers/comment.py
+++ store/serializers/comment.py
@@ -1,15 +1,34 @@
+from django.forms.models import model_to_dict
 from rest_framework import serializers
 
 from store.models import PlantComment, ToolComment
 
 
 class PlantCommentSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField("get_owner")
+    reply_to = serializers.SerializerMethodField("get_reply_to")
+
+    def get_owner(self, obj):
+        return model_to_dict(obj.owner)
+
+    def get_reply_to(self, obj):
+        return model_to_dict(obj.reply_to)
+
     class Meta:
         model = PlantComment
         fields = '__all__'
 
 
 class ToolCommentSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField("get_owner")
+    reply_to = serializers.SerializerMethodField("get_reply_to")
+
+    def get_owner(self, obj):
+        return model_to_dict(obj.owner)
+
+    def get_reply_to(self, obj):
+        return model_to_dict(obj.reply_to)
+
     class Meta:
         model = ToolComment
         fields = '__all__'
