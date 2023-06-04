8c6d333d6b2557b10e789e7c10d1694090b4075f
Amir Mohammad <sohrabi2557@gmail.com>
2023-06-04 18:01:35 +0330

    feat: show full info instead of just id for plant and tool

--- cart/serializers/cart.py
+++ cart/serializers/cart.py
@@ -1,9 +1,15 @@
+from django.forms.models import model_to_dict
 from rest_framework import serializers
 
 from cart.models import PlantCart, ToolCart
 
 
 class PlantCartSerializer(serializers.ModelSerializer):
+    plant = serializers.SerializerMethodField("get_plant")
+
+    def get_plant(self, obj):
+        return model_to_dict(obj.plant)
+
     class Meta:
         model = PlantCart
         exclude = ["is_active", "is_deleted", "deleted_at", "created_at", "updated_at"]
@@ -11,6 +17,11 @@ class PlantCartSerializer(serializers.ModelSerializer):
 
 
 class ToolCartSerializer(serializers.ModelSerializer):
+    tool = serializers.SerializerMethodField("get_tool")
+
+    def get_tool(self, obj):
+        return model_to_dict(obj.tool)
+
     class Meta:
         model = ToolCart
         exclude = ["is_active", "is_deleted", "deleted_at", "created_at", "updated_at"]
