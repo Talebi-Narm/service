686a3f4568fff1bb856278e727c392b2ca30c887
Amir Mohammad <sohrabi2557@gmail.com>
2023-06-05 13:06:30 +0330

    fix: linter errors

--- common/apis/bookmark.py
+++ common/apis/bookmark.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from common.models import PlantBookmark, ToolBookmark
 from common.serializers.bookmark import PlantBookmarkSerializer, ToolBookmarkSerializer
