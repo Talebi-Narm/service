e2309efe2d5c72e536509dc07692d00e99c8d8f2
Hamed Feizabadi <hamedfz19@gmail.com>
2023-07-07 18:52:49 +0330

    fix: coupon url

--- order/urls.py
+++ order/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-# from order.apis import CouponListAPIView, CouponRetrieveAPIView
 from order.apis.order import OrderList, OrderDetail
+from .api import CouponListAPIView, CouponRetrieveAPIView
 
 urlpatterns = [
-    # path('coupons', CouponListAPIView.as_view(), name='coupon-list'),
-    # path('coupons/<uuid:pk>', CouponRetrieveAPIView.as_view(), name='coupon-detail'),
+    path('coupons', CouponListAPIView.as_view(), name='coupon-list'),
+    path('coupons/<uuid:pk>', CouponRetrieveAPIView.as_view(), name='coupon-detail'),
 
     path('order/', OrderList.as_view(), name='order-list'),
     path('order/<uuid:pk>/', OrderDetail.as_view(), name='order-detail')
