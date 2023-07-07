094437b86a5b28be3234937522ed0901e842d6d8
Amir Mohammad <sohrabi2557@gmail.com>
2023-07-07 11:31:05 +0330

    feat: add admin

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Coupon
+from order.models import Coupon, Order
 
 # Register your models here.
 admin.site.register(Coupon)
+admin.site.register(Order)
